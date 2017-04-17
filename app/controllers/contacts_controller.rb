5b71e4b490be842159bc170f9a3ddfea5e24be02
4Chazz <chazzbrooks@gmail.com>
2017-04-17 18:56:47 +0000

    Form now on Cantact new html

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,15 @@
 class ContactsController < ApplicationController
 
   def index
+    @contacts = Contact.all
   end
 
   def show
     @contacts = Contact.new
   end
 
+  def new
+    @contact=Contact.new
+  end
 
 end
