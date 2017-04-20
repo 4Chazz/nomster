0a9433a500c8e4f3a6957f181610fdb24a40fba0
4Chazz <chazzbrooks@gmail.com>
2017-04-20 16:33:38 +0000

    Email send attempt

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "chazzb@ymail.com",
+    mail(to: "chazzfirehose@gmail.com",
           subject: "A comment has been added to your place")
   end
 end
